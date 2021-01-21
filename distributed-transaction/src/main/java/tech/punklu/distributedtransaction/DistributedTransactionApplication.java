7faa5b3267f94d9c0f132837deea1b5ee84fea24
punk1u <punk1u@protonmail.com>
2021-01-21 12:38:01 +0800

    init distributed-transaction demos

---
+++ distributed-transaction/src/main/java/tech/punklu/distributedtransaction/DistributedTransactionApplication.java
@@ -0,0 +1,13 @@
+package tech.punklu.distributedtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTransactionApplication.class, args);
+    }
+
+}
---
+++ distributed-transaction/src/test/java/tech/punklu/distributedtransaction/DistributedTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.punklu.distributedtransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
